fcef9c9f102bdd402b469030a3f1d506431bc238
dunggawr <gawrguraholomyth@gmail.com>
2024-07-31 18:50:31 +0700

    add misson and vision

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -1,3 +1,4 @@
+import { Misson } from "@/app/[locale]/(home)/_sections/misson";
 import { BenefitsSection } from "./_sections/benefits";
 import { FAQSection } from "./_sections/faq";
 import { FeaturesSection } from "./_sections/features";
@@ -8,10 +9,13 @@ import { ServicesSection } from "./_sections/services";
 import { SponsorsSection } from "./_sections/sponsors";
 import { TeamSection } from "./_sections/team";
 import { TestimonialSection } from "./_sections/testimonial";
+import { Vision } from "@/app/[locale]/(home)/_sections/vision";
 
 export default function Home() {
   return (
     <>
+      <Misson />
+      <Vision />
       <HeroSection />
       <SponsorsSection />
       <BenefitsSection />
