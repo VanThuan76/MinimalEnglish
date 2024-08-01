45ac341fd3aa890e141f3ddce3dfb46d6b9e9b44
dunggawr <gawrguraholomyth@gmail.com>
2024-08-01 11:04:25 +0700

    fixed misson vision and add banner

---
+++ app/[locale]/(home)/aboutus/page.tsx
@@ -0,0 +1,11 @@
+import { Misson } from "@/app/[locale]/(home)/aboutus/_sections/misson";
+import { Vision } from "@/app/[locale]/(home)/aboutus/_sections/vision";
+
+export default function AboutUs() {
+  return (
+    <>
+        <Misson />
+        <Vision />
+    </>
+  )
+}
