061b85ddc8fbcc70e8fac96bc28a94c36686bd1d
hotsooc <hotsooc@gmail.com>
2024-08-10 19:32:13 +0700

    update library

--- app/[locale]/(home)/library/_sections/writing.tsx
+++ app/[locale]/(home)/library/_sections/writing.tsx
@@ -11,7 +11,7 @@ type WritingProps = {
 
 export const Writing: React.FC<WritingProps> = ({ title, image, view, time, link }) => {
     return (
-        <div className="relative p-3 bg-[#FDF6EB] border border-[#ffffff] rounded-lg shadow-lg overflow-hidden">
+        <div className="relative p-3 bg-[#ffffff] border border-[#ffffff] rounded-lg shadow-lg overflow-hidden">
             <div className="relative w-full h-[200px] mb-2">
                 <Image
                     src={image}
