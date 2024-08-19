21409b6394c69103ba69f72f52910dcbee6c22a7
hiepnh1312 <125344507+hiepnh1312@users.noreply.github.com>
2024-08-19 16:30:15 +0700

    update class detail

--- app/[locale]/(home)/class/[slug]/_selections/class_detail.tsx
+++ app/[locale]/(home)/class/[slug]/_selections/class_detail.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import { ClassData } from '@/app/[locale]/type';
+import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 
 
-// export const ClassDetail: React.FC<any> = ({class_detail: Class_Detail}) => {
-  export const ClassDetail: React.FC<ClassData> = ({ data_detail}) => {
-
+export const ClassDetail: React.FC<ClassData> = ({ data_detail}) => {
+  const t = useTranslations();
     return (
-      <div className="px-4 py-10 font-sans text-black md:px-[106px] md:py-[40px]">
-        <div className="text-container" dangerouslySetInnerHTML={{ __html: data_detail?.content || '' }} />
-      </div>
+      <>
+        <div className={"w-full flex justify-center text-[#BE5C59] font-[700] text-[16px]"}>
+          {t('class.label.title')}
+        </div>
+        <div className={"w-full flex justify-center text-[#000F30] font-[700] text-[48px] mb-4"}>
+          {data_detail?.name}
+        </div>
+        <div className={"relative w-full h-[428px] md:h-[740px] mb-4"}>
+          <Image
+            src={`/${data_detail?.image}`}
+            alt="image about class"
+            fill
+            style={{
+              objectFit: "cover"
+            }}
+            className={"rounded-[32px]"}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </div>
+        <div className="font-sans text-black md:px-[106px] md:py-[40px]">
+          <div className="text-container" dangerouslySetInnerHTML={{ __html: data_detail?.content || '' }} />
+        </div>
+      </>
+
     );
-  };
+};
 
 export default ClassDetail;
--- app/[locale]/(home)/class/[slug]/page.tsx
+++ app/[locale]/(home)/class/[slug]/page.tsx
@@ -1,13 +1,19 @@
 'use client'
 import ClassDetail from '@/app/[locale]/(home)/class/[slug]/_selections/class_detail';
 import { useGetClassData } from '@/schema/services/class';
+import { useEffect } from 'react';
+import ClassRegister from '@/app/[locale]/(home)/class/[slug]/_selections/class_register';
 
 
 export default function Page({ params }: { params: { slug: string } }) {
-  const class_detail = useGetClassData(params.slug)
+  const class_detail = useGetClassData(params.slug);
+  useEffect(() => {
+    class_detail.refetch()
+  }, [class_detail, params.slug])
   return (
-        <section className='bg-[#FFFFFF] px-[0px] md:px-[80px] pt-3 md:pt-10'>
+        <section className='bg-[#FFFFFF] px-[8px] py-[60px] md:px-[106px] md:py-[80px]'>
           <ClassDetail data_detail={class_detail.data}> </ClassDetail>
+          <ClassRegister></ClassRegister>
         </section>
         
     );
