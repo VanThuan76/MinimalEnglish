35e4bcb0f5cfe3d0d52b496c382af6fda84fbe1c
dunggawr <gawrguraholomyth@gmail.com>
2024-08-22 01:32:03 +0700

    fix css news

--- components/ui/smnews.tsx
+++ components/ui/smnews.tsx
@@ -13,8 +13,8 @@ export const SmNews = ({ image, created_at, read_time, title, tag, description, 
     const [year, month, day] = created_at.split(' ')[0].split('-');
     const publishInfor = `${day}/${month}/${year} - ${read_time} ${t("readtime")}`
     return (
-        <div className='flex flex-col w-full max-w-[390px] gap-6 h-full'>
-            <div className='relative w-full max-w-[390px] h-[200px] rounded-xl overflow-hidden'>
+        <div className='flex flex-col w-full gap-6 h-full'>
+            <div className='relative w-full h-[200px] 2xl:h-[250px] rounded-xl overflow-hidden'>
                 <Image
                     fill
                     style={{ objectFit: "cover" }}
@@ -26,19 +26,19 @@ export const SmNews = ({ image, created_at, read_time, title, tag, description, 
 
             <div className='w-full flex flex-col gap-3 justify-between text-black flex-grow'>
                 <div className='w-full flex justify-between items-center'>
-                    <Badge variant={'secondary'} className='flex justify-center p-2 rounded-lg bg-[#E4E7EC] text-black text-xs font-normal min-w-[74px]'>{tag}</Badge>
-                    <div className='text-base font-normal'>{publishInfor}</div>
+                    <Badge variant={'secondary'} className='flex justify-center p-2 rounded-lg bg-[#E4E7EC] text-black text-xs font-normal min-w-[74px] 2xl:text-base'>{tag}</Badge>
+                    <div className='text-base font-normal 2xl:text-lg'>{publishInfor}</div>
                 </div>
 
-                <div className=' text-2xl font-semibold leading-9 '>
+                <div className='text-2xl font-semibold leading-9 2xl:text-3xl'>
                     {title}
                 </div>
 
-                <div className='text-base font-normal text-[#514F4F]'>
+                <div className='text-base font-normal text-[#514F4F] 2xl:text-lg'>
                     {description}
                 </div>
 
-                <div onClick={() => router.push(`/news&blog/${href}`)} className='flex items-center gap-3 w-fit font-semibold text-[20px] bg-white text-[#000F30] px-0 group/arrow'>
+                <div onClick={() => router.push(`/news&blog/${href}`)} className='flex items-center gap-3 w-fit font-semibold text-[20px] bg-white text-[#000F30] px-0 group/arrow 2xl:text-3xl'>
                     {t("read")}
                     <CircleArrowRight strokeWidth={1.5} size={20} className='group-hover/arrow:translate-x-2 transition-transform' />
                 </div>
