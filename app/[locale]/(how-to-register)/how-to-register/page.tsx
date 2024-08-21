a8c8d1eed268d266c3973e7d06f9194680c15d14
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-08-21 13:49:10 +0700

    #refactor: thuanvv refactor code-v3 Co-authored-by: Hiep <hiepnh1908@gmail.com> Co-authored-by: Hieu <hotsooc@gmail.com> Co-authored-by: Dung <gawrguraholomyth@gmail.com>

--- app/[locale]/(how-to-register)/how-to-register/page.tsx
+++ app/[locale]/(how-to-register)/how-to-register/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import Register from '@/app/[locale]/(how-to-register)/how-to-register/_sections/register';
+import Register from '@/app/[locale]/(how-to-register)/how-to-register/_sections/Register';
 
 export default function Page() {
     return <Register></Register>
