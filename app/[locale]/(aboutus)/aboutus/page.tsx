2b27253dd3706f2d83047259073938633ccb12cc
hiepnh1312 <125344507+hiepnh1312@users.noreply.github.com>
2024-08-22 00:27:46 +0700

    hiepnh update fix bug

--- app/[locale]/(aboutus)/aboutus/page.tsx
+++ app/[locale]/(aboutus)/aboutus/page.tsx
@@ -9,10 +9,12 @@ import { Vision } from "@/app/[locale]/(aboutus)/aboutus/_sections/Vision";
 import { Highlight } from "@/app/[locale]/(aboutus)/aboutus/_sections/Highlights";
 
 import { Feedback } from "@/components/layout/Feedback";
-import { useSectionByUrl } from '@/schema/services/common';
+import { useSectionByPage, useSectionByUrl } from '@/schema/services/common';
 
 export default function AboutUs() {
-    const { data: sections, isLoading } = useSectionByUrl('/aboutus');
+  const { data: sections, isLoading } = useSectionByPage(2);
+
+  // const { data: sections, isLoading } = useSectionByUrl('/aboutus');
 
     if (isLoading) return <></>;
 
